3527d1d49615a9bf7b1be0d9ead3ab9251f84aa3
bunbun88 <sttune8@gmail.com>
2019-05-20 12:23:47 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  get 'tweets'=>'tweets#index'
-  get 'tweets/new'=>'tweets#new'
-  post'tweets'=>'tweets#create'
-  delete 'tweets/:id'=>'tweets#destroy'
-  get   'tweets/:id/edit'  => 'tweets#edit'
-  patch   'tweets/:id'  => 'tweets#update'
+resources :tweets,only:[:index, :new, :create, :destroy, :edit, :update]
+  # get 'tweets'=>'tweets#index'
+  # get 'tweets/new'=>'tweets#new'
+  # post'tweets'=>'tweets#create'
+  # delete 'tweets/:id'=>'tweets#destroy'
+  # get   'tweets/:id/edit'  => 'tweets#edit'
+  # patch   'tweets/:id'  => 'tweets#update'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
